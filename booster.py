8a000240186416d65a8f62adcfab567ff809120f
PaulWattmen <paul.poissonnet@wattmen.org>
2024-11-29 15:58:34 +0100

    version 0.6 : icon bug fixed

--- booster.py
+++ booster.py
@@ -199,7 +199,8 @@ class Booster:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/booster/icon.png'
+
+        icon_path = os.path.join(self.plugin_dir,"icon.png")
         self.add_action(
             icon_path,
             text=self.tr(u'Booster'),
